c743d78ce22abbc71ae0317afdd83044a3d6f4fc
claudy411 <badila.alina@gmail.com>
2022-11-24 16:56:23 +0100

    Claudia- finish header

--- bank-frontend/src/App.js
+++ bank-frontend/src/App.js
@@ -2,19 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Badge from 'react-bootstrap/Badge'
 import DropStartComponent from './js/DropStartComponent';
-
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
-    <div>
-    <h1>
-      Window to switch between register and login{' '}
-      <Badge bg="secondary" as="Button">
-        New
-      </Badge>
-    </h1>
+    <Container fluid>
+   
     <DropStartComponent/>
-  </div>
+  </Container>
   );
 }
 
--- bank-frontend/src/js/DropStartComponent.js
+++ bank-frontend/src/js/DropStartComponent.js
@@ -1,38 +1,84 @@
+import { useState } from "react";
 import Dropdown from "react-bootstrap/Dropdown";
-import DropdownButton from "react-bootstrap/DropdownButton";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import Container from "react-bootstrap/Container";
+
+import "../css/DropStartComponent.css";
 
 const DropStartComponent = () => {
+
+
+  //when click menu change css class
+  const [style, setStyle] = useState(false);
+
+  const open = "justify-content-between button-margin";
+  const closed = "justify-content-between";
+
   return (
-    <Container>
-      <Navbar bg="primary" variant="dark" expand="lg">
-        <Container>
-          <DropdownButton
-            key={"start"}
-            id={`dropdown-button-drop-${"start"}`}
-            drop={"start"}
-            variant="primary"
-          >
-            <Dropdown.Item eventKey="1">Action</Dropdown.Item>
-            <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+    <Navbar bg="primary" variant="dark" className={style ? open : closed}>
+      <div className="dropdown-container">
+        <Dropdown
+          key={"start"}
+          id={`dropdown-button-drop-${"start"}`}
+          drop={"start"}
+          variant="primary"
+        >
+          <Dropdown.Toggle className="ms-5">
+            <span className="display-6">
+              <i
+                className="bi bi-list"
+                onClick={() => {
+                  setStyle(!style);
+                }}
+              ></i>
+            </span>
+          </Dropdown.Toggle>
+          <Dropdown.Menu className=" me-2 ">
+            <Dropdown.Item eventKey="1">
+              <div className="d-flex flex-row">
+                <div className="display-3 me-3 ms-3">
+                  <i className="bi bi-person-vcard-fill"></i>
+                </div>
+                <div className="d-flex flex-column ms-3 mt-2">
+                  <div>nombre</div>
+                  <div>etiqueta</div>
+                </div>
+              </div>
+            </Dropdown.Item>
+            <Dropdown.Item className="mt-5 " eventKey="2">
+              Another action
+            </Dropdown.Item>
             <Dropdown.Item eventKey="3">Something else here</Dropdown.Item>
-            <Dropdown.Divider />
+
             <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
-          </DropdownButton>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-            </Nav>
-          </Navbar>
-        </Container>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+
+      <Navbar.Brand href="#home" className="">
+        <div className="fw-bold fst-italic">
+          <span>
+            <i className="border rounded ps-3 pe-3 me-1 h5 bi bi-currency-dollar"></i>
+          </span>
+          Real Wold App
+        </div>
+      </Navbar.Brand>
+    
+      <Navbar id="basic-navbar-nav">
+        <Nav className="">
+          <Nav.Link
+            href="#home"
+            className="btn btn-success text-uppercase fw-bold ms-3 me-3 ps-3 pe-3"
+          >
+            {" "}
+            <i className=" me-1 bi bi-currency-dollar"></i>New
+          </Nav.Link>
+          <Nav.Link href="#link">
+            <i className="text-white bi bi-bell-fill"></i>
+          </Nav.Link>
+        </Nav>
       </Navbar>
-    </Container>
+    </Navbar>
   );
 };
 

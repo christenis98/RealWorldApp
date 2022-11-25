e864f09c9e085e1167c4a16ab76589dc1d71413d
Ivan <80675645+ivanobishev@users.noreply.github.com>
2022-11-25 11:02:41 +0100

    Ivan - sing up finished

--- bank-frontend/src/App.js
+++ bank-frontend/src/App.js
@@ -7,13 +7,6 @@ import DropStartComponent from './js/DropStartComponent';
 function App() {
   return (
     <div>
-      <h1>
-        Window to switch between register and login{' '}
-        <Badge bg="secondary" as="Button">
-          New
-        </Badge>
-      </h1>
-      <DropStartComponent />
       <Signup />
     </div>
   );
--- bank-frontend/src/js/Signup.jsx
+++ bank-frontend/src/js/Signup.jsx
@@ -1,43 +1,36 @@
-
 import { Form, Button } from "react-bootstrap";
-import dollar_sign from '../img/dollar_sign.png';
 
 function Signup() {
-    return (
-        <div>
-        <header>
-          <img src={dollar_sign} className="h-100" alt="" />
-          Hello World Bank
-          <p>Signing up</p>
-        </header>
-        <Form>
-          <Form.Group className="mt-5 mb-3 w-25 mx-auto">
-            <Form.Label>First name</Form.Label>:
-            <Form.Control type="text" placeholder="First name" />
-          </Form.Group>
-          <Form.Group className="mb-3 w-25 mx-auto">
-            <Form.Label>Second name</Form.Label>:
-            <Form.Control type="text" placeholder="Second name" />
-          </Form.Group>
-          <Form.Group className="mb-3 w-25 mx-auto">
-            <Form.Label>Username</Form.Label>:
-            <Form.Control type="text" placeholder="Username" />
-          </Form.Group>
-          <Form.Group className="mb-3 w-25 mx-auto">
-            <Form.Label>Password</Form.Label>:
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Form.Group className="mb-3 w-25 mx-auto">
-            <Form.Label>Confirm password</Form.Label>:
-            <Form.Control type="password" placeholder="Confirm password" />
-          </Form.Group>
-          
-          <Button className="mt-4">
-            SIGN UP
+  return (
+    <div>
+      <Form>
+        <Form.Group className="mt-5 mb-3 w-25 mx-auto">
+          <Form.Label>First name</Form.Label>:
+          <Form.Control type="text" placeholder="First name" />
+        </Form.Group>
+        <Form.Group className="mb-3 w-25 mx-auto">
+          <Form.Label>Second name</Form.Label>:
+          <Form.Control type="text" placeholder="Second name" />
+        </Form.Group>
+        <Form.Group className="mb-3 w-25 mx-auto">
+          <Form.Label>Username</Form.Label>:
+          <Form.Control type="text" placeholder="Username" />
+        </Form.Group>
+        <Form.Group className="mb-3 w-25 mx-auto">
+          <Form.Label>Password</Form.Label>:
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3 w-25 mx-auto">
+          <Form.Label>Confirm password</Form.Label>:
+          <Form.Control type="password" placeholder="Confirm password" />
+        </Form.Group>
+        <div className="text-center">
+          <Button className="mt-3">
+            Sign up
           </Button>
-        </Form>
-      </div>
-    );
-  }
-
+        </div>
+      </Form>
+    </div>
+  );
+}
 export default Signup;

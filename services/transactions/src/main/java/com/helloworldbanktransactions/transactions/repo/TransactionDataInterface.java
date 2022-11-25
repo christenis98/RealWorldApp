7dd47496ef27913d8031ba7edbc0f971e3c23f90
LinaTsapanou <linatsap32@gmail.com>
2022-11-25 11:54:19 +0100

    Lina- changes

--- services/transactions/src/main/java/com/helloworldbanktransactions/transactions/repo/TransactionDataInterface.java
+++ services/transactions/src/main/java/com/helloworldbanktransactions/transactions/repo/TransactionDataInterface.java
@@ -3,12 +3,14 @@ package com.helloworldbanktransactions.transactions.repo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.helloworldbanktransactions.transactions.model.Transaction;
 
+@Component
 public interface TransactionDataInterface {
 	
-	@Autowired
+	
 	public List<Transaction> getAllTransactions();
 
 

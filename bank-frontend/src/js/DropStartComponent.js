a0e2b7e13c729649f0273550c4c2d621c9ddda0d
claudy411 <badila.alina@gmail.com>
2022-11-25 09:43:11 +0100

    Claudia- navbar finished

--- bank-frontend/src/js/DropStartComponent.js
+++ bank-frontend/src/js/DropStartComponent.js
@@ -6,13 +6,12 @@ import Navbar from "react-bootstrap/Navbar";
 import "../css/DropStartComponent.css";
 
 const DropStartComponent = () => {
-
-
   //when click menu change css class
   const [style, setStyle] = useState(false);
 
   const open = "justify-content-between button-margin";
   const closed = "justify-content-between";
+  let sumAccount = 1681.37;
 
   return (
     <Navbar bg="primary" variant="dark" className={style ? open : closed}>
@@ -23,34 +22,61 @@ const DropStartComponent = () => {
           drop={"start"}
           variant="primary"
         >
-          <Dropdown.Toggle className="ms-5">
-            <span className="display-6">
-              <i
-                className="bi bi-list"
-                onClick={() => {
-                  setStyle(!style);
-                }}
-              ></i>
+          <Dropdown.Toggle className="">
+            <span
+              className="display-6"
+              onClick={() => {
+                setStyle(!style);
+              }}
+            >
+              <i className="bi bi-list"></i>
             </span>
           </Dropdown.Toggle>
           <Dropdown.Menu className=" me-2 ">
-            <Dropdown.Item eventKey="1">
-              <div className="d-flex flex-row">
+            <Dropdown.Header className="dropdown-hover ">
+              <div className="d-flex flex-row dropdown-hover">
                 <div className="display-3 me-3 ms-3">
                   <i className="bi bi-person-vcard-fill"></i>
                 </div>
-                <div className="d-flex flex-column ms-3 mt-2">
-                  <div>nombre</div>
-                  <div>etiqueta</div>
+                <div className="d-flex flex-column ms-3 mt-3">
+                  <div className="text-muted text-capitalize fs-5">nombre</div>
+                  <div className="text-muted fs-5">@etiqueta</div>
                 </div>
               </div>
+            </Dropdown.Header>
+            <Dropdown.Header className="dropdown-hover ">
+              <div className=""></div>
+            </Dropdown.Header>
+            <Dropdown.Header className="dropdown-hover">
+              <div className="fs-4 fw-bold">
+                <i className="text-muted me-1 bi bi-currency-dollar"></i>
+                {sumAccount}
+              </div>
+              <div className="fs-5 text-muted fw-normal  text-capitalize">
+                Account Balance
+              </div>
+            </Dropdown.Header>
+            <Dropdown.Item className="mt-5 text-muted fw-normal mb-3 fs-6 " eventKey="1">
+              <i class="bi bi-house-door-fill me-4"></i>
+              Home
             </Dropdown.Item>
-            <Dropdown.Item className="mt-5 " eventKey="2">
-              Another action
+            <Dropdown.Item eventKey="2" className="text-muted fw-normal mb-3 fs-6">
+              <i class="bi bi-person-fill me-4"></i>
+              My Account
             </Dropdown.Item>
-            <Dropdown.Item eventKey="3">Something else here</Dropdown.Item>
 
-            <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+            <Dropdown.Item eventKey="3" className="text-muted fw-normal mb-3 fs-6">
+              <i class="bi bi-bank2 me-4"></i>
+              Bank Accounts
+            </Dropdown.Item>
+            <Dropdown.Item eventKey="4" className="text-muted fw-normal mb-3 fs-6">
+            <i className=" bi bi-bell-fill me-4"></i>
+              Notifications
+            </Dropdown.Item>
+            <Dropdown.Item eventKey="4" className="text-muted fw-normal mb-3 fs-6 mt-5">
+            <i class="bi bi-box-arrow-right me-4 "></i>
+             Logout
+            </Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
       </div>
@@ -58,22 +84,21 @@ const DropStartComponent = () => {
       <Navbar.Brand href="#home" className="">
         <div className="fw-bold fst-italic">
           <span>
-            <i className="border rounded ps-3 pe-3 me-1 h5 bi bi-currency-dollar"></i>
+            <i className="border rounded ps-3 pe-3 me-1 h5 bi bi-currency-dollar me-3"></i>
           </span>
           Real Wold App
         </div>
       </Navbar.Brand>
-    
+
       <Navbar id="basic-navbar-nav">
         <Nav className="">
           <Nav.Link
             href="#home"
             className="btn btn-success text-uppercase fw-bold ms-3 me-3 ps-3 pe-3"
           >
-            {" "}
             <i className=" me-1 bi bi-currency-dollar"></i>New
           </Nav.Link>
-          <Nav.Link href="#link">
+          <Nav.Link className=" me-5" href="#link">
             <i className="text-white bi bi-bell-fill"></i>
           </Nav.Link>
         </Nav>
